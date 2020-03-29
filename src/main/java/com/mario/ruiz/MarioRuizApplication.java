e8b92c0f5decc84f53832f95c41380a880934686
root <root@localhost.localdomain>
2020-03-29 11:07:29 -0500

    first commit

---
+++ src/main/java/com/mario/ruiz/MarioRuizApplication.java
@@ -0,0 +1,13 @@
+package com.mario.ruiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioRuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioRuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mario/ruiz/MarioRuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mario.ruiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioRuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
